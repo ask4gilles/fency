bef0a2f43402fccd7e6ef018e7a72a797b2cac1a
Gilles Robert <gilles.robert@eurid.eu>
2019-02-11 16:31:04 +0100

    try IT tests on travis.

--- src/test/java/it/io/idempotency/IdempotencyMessagingItTest.java
+++ src/test/java/it/io/idempotency/IdempotencyMessagingItTest.java
@@ -47,7 +47,6 @@ import io.idempotency.MessagingIdempotencyConfig;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.testcontainers.containers.GenericContainer;
@@ -57,7 +56,6 @@ import org.testcontainers.junit.jupiter.Container;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.awaitility.Awaitility.await;
 
-@Disabled("Need to find a way to configure test containers on travis")
 @EnableAutoConfiguration
 @SpringBootTest(classes = {MessagingIdempotencyConfig.class,
     IdempotencyMessagingItTest.TestConfig.class}, properties = {
